6c4cd86a9a2169f1b24fc92d7b19e81ed8166338
abusabir <olegagapitov@gmail.com>
2016-09-30 22:01:04 +0300

    Add link

---
+++ src/ru/abusabir/webapp/Link.java
@@ -0,0 +1,10 @@
+package ru.abusabir.webapp;
+
+/**
+ * Oleg
+ * 30.09.2016
+ */
+public class Link {
+    private String name;
+    private String url;
+}
---
+++ src/ru/abusabir/webapp/Resume.java
@@ -0,0 +1,9 @@
+package ru.abusabir.webapp;
+
+/**
+ * Created by Oleg on 30.09.2016.
+ */
+public class Resume {
+    private String fullName;
+    private String location;
+}
