aebd4c943f8c256beeff611243976a0c4c595d21
abusabir <olegagapitov@gmail.com>
2016-10-05 23:10:05 +0300

    mock Array impl

--- src/ru/abusabir/webapp/model/Resume.java
+++ src/ru/abusabir/webapp/model/Resume.java
@@ -9,9 +9,32 @@ import java.util.List;
  */
 
 public class Resume {
+    private String uuid;
     private String fullName;
     private String location;
     private String homePage;
     private List<Contact> contacts;
     private List<Section> sections;
+
+    public Resume(String fullName, String location) {
+        this.fullName = fullName;
+        this.location = location;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Resume resume = (Resume) o;
+
+        if (!uuid.equals(resume.uuid)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return uuid.hashCode();
+    }
 }
---
+++ src/ru/abusabir/webapp/storage/ArrayStorage.java
@@ -0,0 +1,48 @@
+package ru.abusabir.webapp.storage;
+
+import ru.abusabir.webapp.model.Resume;
+
+import java.util.Collection;
+
+/**
+ * Oleg
+ * 05.10.2016
+ */
+public class ArrayStorage implements IStorage{
+    private Resume[] array = new Resume[100];
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void save(Resume r) {
+
+    }
+
+    @Override
+    public void update(Resume r) {
+
+    }
+
+    @Override
+    public Resume load(String uuid) {
+        return null;
+    }
+
+    @Override
+    public void delete(String uuid) {
+
+    }
+
+    @Override
+    public Collection<Resume> getAllSorted() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
