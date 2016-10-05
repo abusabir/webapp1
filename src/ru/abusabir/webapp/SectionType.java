df485b437b4c9f408cab589e434aa713347fa37a
abusabir <olegagapitov@gmail.com>
2016-10-05 19:53:20 +0300

    Enhance SectionType

--- src/ru/abusabir/webapp/SectionType.java
+++ src/ru/abusabir/webapp/SectionType.java
@@ -5,9 +5,19 @@ package ru.abusabir.webapp;
  * 02.10.2016
  */
 public enum SectionType {
-    OBJECTIVE,
-    ACHIEVEMENT,
-    QUALIFICATIONS,
-    EXPERIENCE,
-    EDUCATION;
+    OBJECTIVE("Позиция"),
+    ACHIEVEMENT("Достижения"),
+    QUALIFICATIONS("Квалификация"),
+    EXPERIENCE("Опыт работы"),
+    EDUCATION("Образование");
+
+    private String title;
+
+    SectionType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
