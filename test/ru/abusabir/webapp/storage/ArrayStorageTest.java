477c1942ffc38132299f5f68b1edbc471e52974b
abusabir <olegagapitov@gmail.com>
2016-10-05 23:31:32 +0300

    add junit lib 4.10

---
+++ test/ru/abusabir/webapp/storage/ArrayStorageTest.java
@@ -0,0 +1,39 @@
+package ru.abusabir.webapp.storage;
+
+public class ArrayStorageTest {
+
+    @org.junit.Test
+    public void testClear() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSave() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testUpdate() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testLoad() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetAllSorted() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSize() throws Exception {
+
+    }
+}
